2db2da72d3f959d5ddba8b360b34370cdff62627
SoftHeartkhum <carolcanqui@gmail.com>
2024-08-04 12:05:06 -0400

    fin modif

--- packages/hardhat/contracts/MyNft.sol
+++ packages/hardhat/contracts/MyNft.sol
@@ -9,7 +9,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	uint256 private _nextTokenId;
 
-	constructor() ERC721("MyToken", "MTK") Ownable() {}
+	constructor(address initialOwner)
+        ERC721("CursoSolidity2024ETHB", "MTK")
+        Ownable()
+    {
+        transferOwnership(initialOwner);
+        mintInitialTokens();
+    }
+	function mintInitialTokens() internal onlyOwner {
+        _mintAndSetURI(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, "https://gateway.pinata.cloud/ipfs/QmSKyYDvxakDqdk9ETpVjUqnuCJx1HS5ZAgQpytigLBybj");
+        _mintAndSetURI(0x70997970C51812dc3A010C7d01b50e0d17dc79C8, "https://gateway.pinata.cloud/ipfs/QmcRw3eqoboCuH7b9RDLFECPmUhkdBFAWWFe5DBXM7UM31");
+	    _mintAndSetURI(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, "https://gateway.pinata.cloud/ipfs/QmWQfLwXcbtETzoWp1uZb7J6TvYzEdmKnhAoguh3fW4xGb");
+		
+    }
+
+    function _mintAndSetURI(address to, string memory uri) internal {
+        safeMint(to, uri);
+    }
 
 	function safeMint(address to, string memory uri) public onlyOwner {
 		uint256 tokenId = _nextTokenId++;
@@ -18,22 +34,33 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	}
 
 	// The following functions are overrides required by Solidity.
+	function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
-	function tokenURI(
-		uint256 tokenId
-	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-		return super.tokenURI(tokenId);
-	}
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-	function supportsInterface(
-		bytes4 interfaceId
-	) public view override(ERC721) returns (bool) {
-		return super.supportsInterface(interfaceId);
-	}
+    function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
 
-	function _burn(
-		uint256 tokenId
-	) internal override(ERC721, ERC721URIStorage) {
-		super._burn(tokenId);
-	}
+    function buyNFT(uint256 tokenId) public payable {
+        require(msg.value >= 0.1 ether, "Insufficient payment");
+        address owner = ownerOf(tokenId);
+        require(owner != msg.sender, "Cannot buy your own NFT");
+        
+        _transfer(owner, msg.sender, tokenId);
+    }
 }
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import Link from "next/link";
-import data from "./listNfts.json";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import ClearImage from "~~/components/cards/ClearImage";
 import Carousel from "~~/components/carousels/Carousel";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
@@ -15,10 +11,10 @@ const Home: NextPage = () => {
   return (
     <>
       <Carousel />
-      <div className="flex grid-cols-4 gap-4">
-        {data.map(e => (
-          <ClearImage imageUri={e.imageUri} title={e.title} description={e.description} />
-        ))}
+      <div className="flex flex-wrap gap-4 p-5">
+        <ClearImage tokenId={0} address={connectedAddress} />
+        <ClearImage tokenId={1} address={connectedAddress} />
+        <ClearImage tokenId={2} address={connectedAddress} />
       </div>
     </>
   );
